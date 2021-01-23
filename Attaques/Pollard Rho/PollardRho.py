efdd16d15b32558256c66e0bb814225c9f59922d
ViCauj <77888330+ViCauj@users.noreply.github.com>
2021-01-23 17:30:53 +0100

    Update PollardRho.py

--- Attaques/Pollard Rho/PollardRho.py
+++ Attaques/Pollard Rho/PollardRho.py
@@ -46,30 +46,31 @@ def Walk(trouple, cste):
         return ((x**2)%p, (2*a)%r, (2*b)%r)
     return ((x*Lg[j])%p, (a+u[j])%r, (b+v[j])%r)
         
-def PollardRho(p, g, n, h):
+def PollardRho(p, g, n, h, r = -1):
     '''
     g dans Z/pZ,
     G = <g>,
     n le nombre de subdivisions Si de G = S0 U S1 U ... U Sn-1,
     h = g^a avec a le truc qu'on cherche (a = logg(h)) et h dans G
+    r l'ordre de g que l'on peut donner pour accelerer l'algo si on le souhaite
     '''
-    G, r = Fp(p, g)
+    if r == -1:
+        G, r = Fp(p, g)
     
     ############ CHOIX DE LA MARCHE  A L E A T O I R E !!!!!!!!!!! ############
-    u, v = rds(0, r, n), rds(0, r, n)
-    u, v = [0, 37, 71, 76], [0, 34, 69, 18]
-    Lg = [(((g**u[i])%p)*((h**v[i])%p))%p for i in range(n)]
-    print(Lg, "\n")
+    #u, v = list(rds(0, r, n)), list(rds(0, r, n))
+    u = [rds(0,r) for i in range(n)]
+    v = [rds(0,r) for i in range(n)]
+    Lg = [ (((g**u[i])%p)*((h**v[i])%p))%p for i in range(n)]
     cste = Lg, u, v, n, p, r
     trouple1 = (g, 1, 0)
     trouple2 = Walk(trouple1, cste)
     ########## FIN CHOIX DE LA MARCHE  A L E A T O I R E !!!!!!!!!!! ##########
     
-    print(trouple1, S(trouple1[0], n), "          ", trouple2, S(trouple2[0], n))
     while trouple1[0] != trouple2[0]:
         trouple1 = Walk(trouple1, cste)
         trouple2 = Walk(Walk(trouple2, cste), cste)
-        print(trouple1, S(trouple1[0], n), "          ", trouple2, S(trouple2[0], n))
     if trouple1[2]%r == trouple2[2]%r:
         return -1
-    return ((trouple2[1]-trouple1[1])*inv(trouple1[2]-trouple2[2], p))%r
+    res = ((trouple2[1]-trouple1[1])*inv(trouple1[2]-trouple2[2], r))%r
+    return res, (g**res)%p
