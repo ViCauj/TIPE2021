902d37421276d2f720f9ddaec75322c6b40513ab
ViCauj <77888330+ViCauj@users.noreply.github.com>
2021-01-23 18:17:32 +0100

    Update BirthdayParadox.py

--- Attaques/Pollard Rho/BirthdayParadox.py
+++ Attaques/Pollard Rho/BirthdayParadox.py
@@ -1,1 +1,21 @@
+from numpy.random import randint as rd
+import matplotlib.pyplot as plt
 
+
+N=10000
+n=500
+res=[]
+moy=[]
+lim=[(3.14*N/2)**0.5 + 2 for i in range(n)]
+
+for i in range(n):
+    res.append(1)
+    S=[rd(0,N)]
+    while not S[-1] in S[:-1]:
+        S.append(rd(0,N))
+        res[-1]+=1
+    moy.append(sum(i for i in res)/len(res))
+    
+plt.plot(list(range(n)),res,"grey")
+plt.plot(list(range(n)),moy)
+plt.plot(list(range(n)),lim,"r")
