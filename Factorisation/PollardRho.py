bee65094ad424f89ce7ee104c51089052ad2beb0
ViCauj <77888330+ViCauj@users.noreply.github.com>
2021-02-17 18:26:30 +0100

    Create PollardRho.py

---
+++ Factorisation/PollardRho.py
@@ -0,0 +1,34 @@
+def prod(L):
+    r=1
+    for i in L:
+        r*=i
+    return r
+
+def PollardFact(N,k=1):
+    def f(x):
+        return x**2 + k
+    a=2
+    b=f(a)%N
+    d=0
+    while not(1<d<N):
+        a=f(a)%N
+        b=f(f(b))%N
+        d=PGCD(b-a,N)
+    return d
+
+def listeD(N):
+    n=N
+    D=[]
+    while prod(D)!=N:
+        if Prime(n):
+            D.append(n)
+        else:
+            r=PollardFact(n)
+            # if Prime(r):
+            #     D.append(r)
+            # else:
+            #     D.append(listeD(r))
+            D.append(r)
+            n//=D[-1]
+    return D
+        
