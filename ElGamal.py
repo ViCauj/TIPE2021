f75bfb47677fd541a9da17758971e52349e7c9e3
ViCauj <77888330+ViCauj@users.noreply.github.com>
2021-02-17 18:56:26 +0100

    Update ElGamal.py

--- ElGamal.py
+++ ElGamal.py
@@ -4,8 +4,7 @@ def clePub(a,p,g):
     return (p,g,g**a%p)     
 
 def cryptCar(car,a,p,g):
-    return (g**cleEph%p,ord(car)*
-                        (clePub(a,p,g)[2]**cleEph)%p)
+    return (g**cleEph%p,ord(car)*(clePub(a,p,g)[2]**cleEph)%p)
 
 def deCryptCar(carChif,a,p):
     return chr((carChif[0]**(p-a-1)%p)*carChif[1]%p)
