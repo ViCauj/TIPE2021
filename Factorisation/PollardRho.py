4f33e2a0a5beffedde49871544fc85c4c7679975
ViCauj <77888330+ViCauj@users.noreply.github.com>
2021-02-17 18:28:42 +0100

    Update PollardRho.py

--- Factorisation/PollardRho.py
+++ Factorisation/PollardRho.py
@@ -1,34 +1,34 @@
 def prod(L):
-    r=1
+    r = 1
     for i in L:
-        r*=i
+        r *= i
     return r
 
-def PollardFact(N,k=1):
+def PollardFact(N, k = 1):
     def f(x):
         return x**2 + k
-    a=2
-    b=f(a)%N
-    d=0
-    while not(1<d<N):
-        a=f(a)%N
-        b=f(f(b))%N
-        d=PGCD(b-a,N)
+    a = 2
+    b = f(a)%N
+    d = 0
+    while not 1 < d < N :
+        a = f(a)%N
+        b = f(f(b))%N
+        d = PGCD(b-a,N)
     return d
 
 def listeD(N):
-    n=N
-    D=[]
-    while prod(D)!=N:
+    n = N
+    D = []
+    while prod(D) != N:
         if Prime(n):
             D.append(n)
         else:
-            r=PollardFact(n)
+            r = PollardFact(n)
             # if Prime(r):
             #     D.append(r)
             # else:
             #     D.append(listeD(r))
             D.append(r)
-            n//=D[-1]
+            n //= D[-1]
     return D
         
