5d482e285d6d649880d360f0ee33dee7ea0a8ac7
ViCauj <77888330+ViCauj@users.noreply.github.com>
2021-03-14 14:21:36 +0100

    Create TrouveGroupe.py

---
+++ Benchmark/TrouveGroupe.py
@@ -0,0 +1,12 @@
+from numpy.random import randint as rd
+
+def trouveG(listeD, p):
+    res = 0
+    test = True
+    while test:
+        g = rd(1, p)
+        for li in listeD:
+            if (g**li)%p == 1:
+                res = (g, li)
+                test = False
+    return res
