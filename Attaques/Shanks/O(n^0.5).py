abc39aec70b9d09419aa1ae74e6b570095b5f4ed
ViCauj <77888330+ViCauj@users.noreply.github.com>
2021-03-30 16:31:47 +0200

    Update O(n^0.5).py

--- Attaques/Shanks/O(n^0.5).py
+++ Attaques/Shanks/O(n^0.5).py
@@ -1,11 +1,12 @@
-def Shanks_en_mieux(A, g, p, r):
-    s = 1 + int(np.sqrt(p))
-    b = euclEtend(g**s,p)[1]%p
+def Shanks_en_mieux(h, g, p, r):
+    s = 1 + int(p**0.5)
+    b = euclEtend(expoRapMod(g, s, p),p)[1]%p
     d1 = {1:0}
     for i in range(1, s + 1): #on crée le dictionnaire#
-        d1[(g**i)%p] = i # les elts de L1 sont les clefs#
+        d1[expoRapMod(g, i, p)] = i # les elts de L1 sont les clefs#
     for i in range(s + 1):#recherche d'une occurrence#
-        if ((A*b**i)%p) in d1: # commune#
-            x, y = d1[(A*b**i)%p], i
+        k = expoRapMod(h*b, i, p)
+        if k in d1: # commune#
+            x, y = d1[k], i
             return (x + y*s)%r  #on stoppe l'algorithme #
             #quand on trouve la solution#
