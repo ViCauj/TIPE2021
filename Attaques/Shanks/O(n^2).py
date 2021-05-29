7b66af4a75a50a91fbc4886941a0ce2e98212d13
ViCauj <77888330+ViCauj@users.noreply.github.com>
2021-05-29 12:23:25 +0200

    Update O(n^2).py

--- Attaques/Shanks/O(n^2).py
+++ Attaques/Shanks/O(n^2).py
@@ -3,11 +3,13 @@ def occurence_commune(L1, L2):
         for j in range(len(L2)):
             if L1[i] == L2[j]:
                 return (i, j)
-
+            
 def Shanks2(p, g, h, r):
-    s = 1 + int(p**0.5)
+    h = h%p
+    s = 1 + int(r**0.5)
     b = euclEtend(expoRapMod(g, s, p), p)[1]%p
     L1 = [1] + [expoRapMod(g, i, p) for i in range(1,s+1)]
-    L2 = [expoRapMod(h*b, i, p) for i in range(s+1)]
+    L2 = [h*expoRapMod(b, i, p)%p for i in range(s+1)]
     x, y = occurence_commune(L1,L2)
     return (x + y*s)%r
+
